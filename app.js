3a95ab80eb9dd392c365b7fb09882720bb133177
Pablo Ricardo <pabloricardo055@gmail.com>
2024-11-06 17:56:53 -0300

    aula dia 06/11

--- app.js
+++ app.js
@@ -9,17 +9,22 @@ const porta = 5600
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
+//CONFIGURAR HANDLEBARS
+app.engine('handlebars',handlebars.engine({extended:true}))
+app.set('view engine', 'handlebars') // definindo o handlebars como mecanismo de visualização padrão.
+
 // CARREGAR AS PORTAS
 const funcionarioRouter = require('./routes/funcionario')
 
 // UTILIZANDO AS ROTAS 
-app.use('/funcionario', funcionarioRouter)
+app.use('/funcionario',funcionarioRouter)
 
 // EXIBINDO INFORMAÇÕES NA TELA
-app.get("/", (req, res)=>{
-    res.send("<h1>Tudo funcionando</h1>")
-})
+app.get("/",(req, res)=>{
+    //res.send("<h1>Tudo funcionando</h1>")
+    res.render('home')
 
+})
 
 
 
--- routes/funcionario.js
+++ routes/funcionario.js
@@ -7,13 +7,20 @@ const departamento = require('../models/departamento')
 
 //CRIANDO AS ROTAS 
 //1ª ROTA - INSERIR DADOS NA TABELA
-router.get('/store',async(req, res)=>{
+router.post('/store',async(req, res)=>{
     const resultado = await funcionario.create({
-        nome:'Joana Dark',
-        salario:3000,
-        cargo:'Supervisora',
-        departamentoId:1 // Esse campo é a chave estrangeira
+        nome:req.body.nome,
+        salario:req.body.salario,
+        cargo:req.body.cargo,
+        departamentoId:req.body.departamento // Esse campo é a chave estrangeira
     })
+
+    if(resultado){
+        res.redirect('/')
+    }
+    else(
+        res.json({erro:"Os dados não foram cadastrados no banco"})
+    )
 })
 
 // 2ª ROTA - EXIBIR A PÁGINA INICIAL DO FUNCiONÁRIO
@@ -33,4 +40,18 @@ router.get('/show',async(req, res)=>{
     }
 })
 
+// 4ª ROTA - DELETAR DADOS DA TABELA
+// :id significa que iremos passar um valor na rota, ou seja, iremos informar um valor que poderá ser diferente e que será armazenado pela variável :id
+router.get('/destroy/:id',async(req, res)=>{
+    const resultado = await funcionario.destroy({
+        where:{
+            id:req.params.id // estamos recebendo o id via parâmetro que está sendo passado na rota, no caso,é o id que estamos recebendo
+        }
+    })
+})
+
+//5ª ROTA - EXIBIR FORMULÁRIO DE CADASTRO
+router.get('/create',(req, res)=>{
+    res.render('funcionario/addFuncionario')
+})
 module.exports = router
